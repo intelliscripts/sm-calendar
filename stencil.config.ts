b84ccd151e7a8ab8b6c26e1db9bcdd995876f1b8
shivaprasad.manupadi <shivaprasad.manupadi@servicenow.com>
2019-11-17 13:45:48 +0530

    stencil config

--- stencil.config.ts
+++ stencil.config.ts
@@ -12,8 +12,7 @@ export const config: Config = {
   ],
   outputTargets: [
     {
-      type: 'dist',
-      esmLoaderPath: '../loader'
+      type: 'dist'
     },
     {
       type: 'docs-readme'
